196604622f4f7f01efe7dcc7ae3fbd55b38a6e5a
siva guru <sivabaddie@gmail.com>
2022-10-18 17:21:14 +0530

    sample code to get stock data

---
+++ data/getData.py
@@ -0,0 +1,28 @@
+
+
+'''
+install the followign 
+pip install yfinance
+pip install pendulum
+pip install matplotlib
+'''
+
+
+
+##Sample code to download minute level data
+# Import package
+import yfinance as yf
+
+# valid periods: 1d,5d,1mo,3mo,6mo,1y,2y,5y,10y,ytd,max
+# valid intervals: 1m,2m,5m,15m,30m,60m,90m,1h,1d,5d,1wk,1mo,3mo
+# last one minute data is available only for 7 days
+# 5m, 15, and 30m are availavble for 60 days
+
+# Get the data
+data = yf.download(tickers="MARUTI.NS", period="3mo", interval="60m")
+
+# Print the data
+print("Printing tail")
+print(data.tail())
+print("Printing head")
+print(data.head())
