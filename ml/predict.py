f940cf669fe39d84acb589897e26ef51c7e2c5e8
sivaguru <sivabaddie@gmail.com>
2022-11-13 01:30:09 +0530

    added data check and traning class

--- ml/predict.py
+++ ml/predict.py
@@ -1,12 +1,19 @@
 
+from train import Traning
+import os
+from tensorflow import keras
+
+dirname = os.path.dirname(__file__)
+
 
 class Predict():
     def __init__(self) -> None:
-        Tra
+        self.train = Traning()
         pass
     
-    def LoadModel(self):
-        pass
+    def loadModel(self, modelPath):
+        return keras.models.load_model(self.modelPath)
+
     
     def LoadData(self):
         pass
@@ -14,5 +21,21 @@ class Predict():
     def StartTraning(self):
         pass
 
-    def PredictData(self):
-        pass
+    def CheckData(self, modelPath):
+        if os.path.exists(modelPath):
+            return True
+        return False
+
+    def PredictData(self, name):
+        stockPath = os.path.join(dirname, 'stock', name)
+        modelpath = os.path.join(self.stockPath, name + ".pickel")
+        stockCsvPath = os.path.join(self.stockPath, name + ".csv")
+
+        model = None
+        if self.CheckData(modelpath):
+            model = self.loadModel(modelpath)
+        else:
+            model = self.train.StartTraning(name, stockPath, modelpath, stockCsvPath)
+
+        ##predict data with the model        
+        pass    
