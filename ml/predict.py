275c01360e95a274509a4a6e44e0163c33e75fb1
sivaguru <sivabaddie@gmail.com>
2022-11-13 01:17:08 +0530

    predict class init

---
+++ ml/predict.py
@@ -0,0 +1,18 @@
+
+
+class Predict():
+    def __init__(self) -> None:
+        Tra
+        pass
+    
+    def LoadModel(self):
+        pass
+    
+    def LoadData(self):
+        pass
+    
+    def StartTraning(self):
+        pass
+
+    def PredictData(self):
+        pass
