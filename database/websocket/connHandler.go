b46970e75aba1fd258bc4dbaa3cad1f581338a89
ssivaguru <sivabaddie@gmail.com>
2022-11-26 17:11:48 +0530

    websocket Message Handling

--- database/websocket/connHandler.go
+++ database/websocket/connHandler.go
@@ -1,9 +1,13 @@
 package websocket
 
-import "github.com/gorilla/websocket"
+import (
+	"sync"
+
+	"github.com/gorilla/websocket"
+)
 
 type ConnHandler interface {
-	ReadMessage() (error, string)
+	ReadMessage() (error, []byte)
 	WriteMessage(data []byte) error
 	CloseConnection() error
 }
@@ -16,18 +20,23 @@ func CreateNewConn(conn *websocket.Conn) *wsConn {
 	return &wsConn{conn}
 }
 
-func (ws *wsConn) ReadMessage() (error, string) {
+func (ws *wsConn) ReadMessage() (error, []byte) {
 	_, data, err := ws.conn.ReadMessage()
 
 	if err != nil {
-		return err, ""
+		return err, nil
 	}
 
-	return nil, string(data[:])
+	return nil, data
 }
 
+var mu sync.Mutex
+
 func (ws *wsConn) WriteMessage(data []byte) error {
-	return ws.conn.WriteMessage(websocket.TextMessage, data)
+	mu.Lock()
+	err := ws.conn.WriteMessage(websocket.TextMessage, data)
+	mu.Unlock()
+	return err
 }
 
 func (ws *wsConn) CloseConnection() error {
