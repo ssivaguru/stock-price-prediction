4811db6da47e6dc59244a96d9147205be5e8177f
ssivaguru <sivabaddie@gmail.com>
2022-12-10 11:49:30 +0530

    handle put message

--- database/database/database.go
+++ database/database/database.go
@@ -17,6 +17,8 @@ type DbInterface interface {
 	InsertData(task *Stock) error
 	GetData(name string) (*Stock, error)
 	DeleteStock(text string) error
+	UpdateByName(name string, predict []float64) (*mongo.UpdateResult, error)
+	UpdateState(name string, state int) (*mongo.UpdateResult, error)
 }
 
 type DbStruct struct {
@@ -25,23 +27,25 @@ type DbStruct struct {
 }
 
 type Stock struct {
-	ID        primitive.ObjectID `bson:"_id"`
-	Name      string             `bson:"name"`
-	Path      string             `bson:"path"`
-	Status    uint               `bson:"status"`
-	UpdatedAt time.Time          `bson:"updated_at"`
+	ID              primitive.ObjectID `bson:"_id"`
+	Name            string             `bson:"name"`
+	Path            string             `bson:"path"`
+	Status          uint               `bson:"status"`
+	UpdatedAt       time.Time          `bson:"updated_at"`
+	Requesting_user []string           `bson:"requesting_user"`
+	Predicted_val   []float64          `bson:"predicted_val"`
 }
 
-var ctx = context.TODO()
-
 func ConnectDb() DbInterface {
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI("mongodb://localhost:27017"))
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://localhost:27017"))
 	if err != nil {
 		log.Println(err)
 		return nil
 	}
 
-	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
 		log.Println(err)
 		return nil
 	}
@@ -54,6 +58,8 @@ func ConnectDb() DbInterface {
 }
 
 func (db *DbStruct) InsertData(task *Stock) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
 	_, err := db.collection.InsertOne(ctx, task)
 	return err
 }
@@ -66,7 +72,50 @@ func (db *DbStruct) GetData(name string) (*Stock, error) {
 	return db.fileterData(filter)
 }
 
+func (db *DbStruct) UpdateState(name string, state int) (*mongo.UpdateResult, error) {
+	filter := bson.D{
+		primitive.E{Key: "name", Value: name},
+	}
+
+	update := bson.D{
+		primitive.E{"$set",
+			bson.D{
+				primitive.E{Key: "status", Value: state},
+			},
+		}}
+
+	result, err := db.collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		panic(err)
+	}
+
+	return result, nil
+}
+
+func (db *DbStruct) UpdateByName(name string, prediction []float64) (*mongo.UpdateResult, error) {
+
+	filter := bson.D{
+		primitive.E{Key: "name", Value: name},
+	}
+
+	update := bson.D{
+		primitive.E{"$set",
+			bson.D{
+				primitive.E{Key: "predicted_val", Value: prediction},
+			},
+		}}
+
+	result, err := db.collection.UpdateOne(context.TODO(), filter, update)
+	if err != nil {
+		panic(err)
+	}
+
+	return result, nil
+}
+
 func (db *DbStruct) DeleteStock(text string) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
 	filter := bson.D{primitive.E{Key: "name", Value: text}}
 
 	res, err := db.collection.DeleteOne(ctx, filter)
@@ -81,6 +130,8 @@ func (db *DbStruct) DeleteStock(text string) error {
 }
 
 func (db *DbStruct) fileterData(filter interface{}) (*Stock, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
 	// A slice of tasks for storing the decoded documents
 	var tasks *Stock
 
--- database/dbServer.go
+++ database/dbServer.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"stock-price-prediction/database/database"
 	"time"
@@ -23,11 +24,13 @@ func (s *DbServer) handlePredict(c *gin.Context) {
 
 	bodyMap := c.Request.URL.Query()
 	log.Println(bodyMap)
+
 	if _, ok := bodyMap["name"]; !ok {
 		c.JSON(400, "{Msg: Malformed URL}")
 		return
 	}
 
+	fmt.Println(" predicting for data ", bodyMap.Get("name"))
 	//jsut check if its already created
 	data, err := s.Dbclient.GetData(bodyMap.Get("name"))
 
@@ -43,7 +46,7 @@ func (s *DbServer) handlePredict(c *gin.Context) {
 		return
 	}
 
-	err = s.Dbclient.InsertData(&database.Stock{ID: primitive.NewObjectIDFromTimestamp(time.Now()), Name: bodyMap.Get("name"), Path: "", Status: StatusTraning, UpdatedAt: time.Now()})
+	err = s.Dbclient.InsertData(&database.Stock{ID: primitive.NewObjectIDFromTimestamp(time.Now()), Name: bodyMap.Get("name"), Path: "", Status: StatusTraning, UpdatedAt: time.Now(), Requesting_user: nil, Predicted_val: nil})
 
 	if err != nil {
 		log.Println("handleCreateStock:: error creating stock ", err)
@@ -61,6 +64,24 @@ func (s *DbServer) handleDeleteStock(c *gin.Context) {
 }
 
 func (s *DbServer) handleUpdateStock(c *gin.Context) {
+	var buffer map[string]interface{}
+
+	json.NewDecoder(c.Request.Body).Decode(&buffer)
+
+	log.Println(buffer)
+
+	respos := buffer["predict"].([]interface{})
+	var prediction []float64
+
+	for _, val := range respos {
+		prediction = append(prediction, val.(float64))
+	}
+	fmt.Println(prediction)
+
+	s.Dbclient.UpdateByName(buffer["name"].(string), prediction)
+	s.Dbclient.UpdateState(buffer["name"].(string), StatusReady)
+
+	c.Data(200, gin.MIMEJSON, getJson(map[string]string{"status": " updating value"}))
 }
 
 func (s *DbServer) setupRoutes(router *gin.Engine) {
@@ -85,6 +106,8 @@ func (s *DbServer) setupRoutes(router *gin.Engine) {
 func (s *DbServer) start() error {
 
 	router := gin.Default()
+	fmt.Println("setting default route")
 	s.setupRoutes(router)
+	fmt.Println("starting router")
 	return router.Run("localhost:8080")
 }
